72af2e1863a6f26d68a6e01331f9c31a9efacaa0
NARUTO007-max <nu3762800@gmail.com>
2025-06-12 05:18:50 +0530

    Create db.py

---
+++ waifu/db.py
@@ -0,0 +1,10 @@
+#database.py
+from motor.motor_asyncio import AcyncIOMotorClient
+#MONGO_CLIENT
+MONGO_URI = "mongodb+srv://sufyan532011:2011@bey-wars.oji9phy.mongodb.net/?retryWrites=true&w=majority&appName=Bey-Wars"
+monogo_client = AsyncIOMotorClient(MONGO_URI)
+DB = mongo_client["BEYBLADE"]
+#All Database
+users = DB["USERS"]
+BannedUser = DB["BANNED USERS"]
+guilds = DB["GUILDS"]
