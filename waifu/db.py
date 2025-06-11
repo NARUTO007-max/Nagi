eaf66de47aba7e78b6f8d44502cab424b91e6f18
NARUTO007-max <nu3762800@gmail.com>
2025-06-12 05:19:28 +0530

    Update db.py

--- waifu/db.py
+++ waifu/db.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AcyncIOMotorClient
 #MONGO_CLIENT
 MONGO_URI = "mongodb+srv://sufyan532011:2011@bey-wars.oji9phy.mongodb.net/?retryWrites=true&w=majority&appName=Bey-Wars"
 monogo_client = AsyncIOMotorClient(MONGO_URI)
-DB = mongo_client["BEYBLADE"]
+DB = mongo_client["Waifu"]
 #All Database
 users = DB["USERS"]
 BannedUser = DB["BANNED USERS"]
