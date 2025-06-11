7b281308536a754f524c8a7b1d9a7853b9abab7e
NARUTO007-max <nu3762800@gmail.com>
2025-06-12 05:18:06 +0530

    Create __main__.py

---
+++ waifu/__main__.py
@@ -0,0 +1,10 @@
+#__main__.py
+from pyrogram import Client, filters
+from Waifu import bot
+from Modules import *
+
+if __name__=="__main__":
+    bot.run()
+    with bot:
+        await bot.send_message(chat_id=7576729648,
+                               text="BOT IS STARTED")
