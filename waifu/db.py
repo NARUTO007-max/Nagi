af36a7b7722cc2968c737111e165fad8efcef2eb
NARUTO007-max <nu3762800@gmail.com>
2025-06-17 00:25:39 +0530

    Update db.py

--- waifu/db.py
+++ waifu/db.py
@@ -1,7 +1,7 @@
 #database.py
 from motor.motor_asyncio import AcyncIOMotorClient
 #MONGO_CLIENT
-MONGO_URI = "mongodb+srv://sufyan532011:2011@bey-wars.oji9phy.mongodb.net/?retryWrites=true&w=majority&appName=Bey-Wars"
+MONGO_URI = "mongodb+srv://sufyan532011:5042@auctionbot.5ms20.mongodb.net/?retryWrites=true&w=majority&appName=AuctionBot"
 monogo_client = AsyncIOMotorClient(MONGO_URI)
 DB = mongo_client["Waifu"]
 #All Database
