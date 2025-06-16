e0a05fd152d739043302e5bc232a71c338b7aa76
NARUTO007-max <nu3762800@gmail.com>
2025-06-17 00:24:16 +0530

    Update __init__.py

--- waifu/__init__.py
+++ waifu/__init__.py
@@ -1,1 +1,19 @@
-# __init__.py
+# waifu/__init__.py
+
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+import logging
+
+bot = Client(
+    "BeybladeBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="waifu/Modules")
+)
+
+logging.basicConfig(
+  format="[KuroAI-Beta] %(name)s - %(levelname)s - %(message)s",
+  handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+  level=logging.INFO,
+)
