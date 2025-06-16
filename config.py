ee8847015f91e8203944c5910f9a91d627580b67
NARUTO007-max <nu3762800@gmail.com>
2025-06-17 00:27:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
+# config.py
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URL = os.getenv("MONGO_URL")
+API_ID = os.getenv("API_ID", 21218274)
+API_HASH = os.getenv("API_HASH", "3474a18b61897c672d315fb330edb213")
+BOT_TOKEN = os.getenv(" TOKEN", "7207574948:AAGRV7Te9gcinmy9hLnPvAOiEzcOYgllDns")
+ADMINS = [7576729648, 6239769036]
